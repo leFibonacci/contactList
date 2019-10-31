b13ce8827b4bde3cb00bb1f30d780510fcc92049
leFibonacci <dilaraeceozturk@gmail.com>
2019-10-31 21:36:07 +0300

    Digit only directive tamamlandı

--- src/app/digit-only.directive.ts
+++ src/app/digit-only.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, Inject, ElementRef } from '@angular/core';
+import { Directive, Inject, ElementRef, HostListener, Self, EventEmitter, Output, OnInit, NgZone } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Directive({
   selector: '[appDigitOnly]'
 })
 export class DigitOnlyDirective {
-  inputElement: HTMLInputElement;
-  constructor(ref: ElementRef) { 
-    this.inputElement=ref.nativeElement;
+
+  constructor(private model: NgModel) {
+  }
+
+  @HostListener("keyup", ['$event'])
+  checkifInputIsDigit(event) {
+    const newValue = this.model.value.replace(/\D/g, '');
+      this.model.valueAccessor.writeValue(newValue);   
   }
 
 }
